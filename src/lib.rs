94a8cae89f7e338e6a6fd8d9e25a7a20d6f274ac
lucidust <lucidust@gmail.com>
2022-04-03 02:29:36 +0900

    Improve error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
-pub fn parse_args(args: &[String]) -> u32 {
+use std::error::Error;
+
+pub fn parse_args(args: &[String]) -> Result<u32, &'static str> {
+    if args.len() < 2 {
+        return Err("not enough arguments");
+    }
+
     let arg1: String = String::from(&args[1]);
-    arg1.parse::<u32>().unwrap()
+
+    Ok(arg1.parse::<u32>().unwrap())
 }
 
-pub fn print_vec(vec: &Vec<u32>) {
+pub fn print_vec(vec: &Vec<u32>) -> Result<(), Box<dyn Error>> {
     for index in 0..vec.len() {
         print!("[{:^4}], ", vec[index]);
 
@@ -13,4 +20,6 @@ pub fn print_vec(vec: &Vec<u32>) {
     }
 
     println!();
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use rust_eratosthenes;
 mod eratos;
-use std::env;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let n: u32 = rust_eratosthenes::parse_args(&args);
+    let n: u32 = rust_eratosthenes::parse_args(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     if eratos::is_prime_number(n) {
         println!("{} is a prime number.", n);
@@ -31,7 +33,11 @@ fn main() {
         );
         let prime_numbers: Vec<u32> = eratos::get_prime_numbers_below(n);
         println!("Prime numbers less than {}.", n);
-        rust_eratosthenes::print_vec(&prime_numbers);
+
+        if let Err(e) = rust_eratosthenes::print_vec(&prime_numbers) {
+            println!("Application error: {}", e);
+            process::exit(1);
+        };
     } else {
         println!("There is no prime number less than {}.", n);
     }
